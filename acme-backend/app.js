2c670d093bd455cb023ba7d41e88f0afc5551293
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-04-09 08:04:39 +0200

    Adjusting client port to avoid ALlow-Cross origin

--- acme-backend/app.js
+++ acme-backend/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 
 // Add headers
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
